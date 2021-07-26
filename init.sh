58b3287a9975f2dc28f1911903eca47ea692fe27
unclebob2008 <unclebob@rambler.ru>
2021-07-26 09:12:52 +0000

    init v1.2

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-/etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
 #ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #/etc/init.d/gunicorn restart
 #/etc/init.d/mysql start
