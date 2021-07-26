ed1fa0af7d94d36398abd67ce89ce6f83809a350
unclebob2008 <unclebob@rambler.ru>
2021-07-26 13:02:24 +0000

    Revert "Remove log conf"
    This reverts commit 30c4b3f4daee2a704f0c80cdf6a193be0106394b.

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
