f8b168a8f8caf2d3d7284f0266b4717cdffe5c9c
unclebob2008 <unclebob@rambler.ru>
2021-07-26 08:13:48 +0000

    Directory structur + nginx conf v1

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
+ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+/etc/init.d/nginx restart
+#ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#/etc/init.d/gunicorn restart
+#/etc/init.d/mysql start
