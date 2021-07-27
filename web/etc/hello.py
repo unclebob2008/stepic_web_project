2313596933e8dad2600d9da67b4ef1bfd4a5eae9
unclebob2008 <unclebob@rambler.ru>
2021-07-27 12:10:16 +0000

    Added hello.py

---
+++ web/etc/hello.py
@@ -0,0 +1,7 @@
+def app(environ, start_response):
+        data = "\n".join(environ.get('QUERY_STRING').split("&"))
+        start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(data)))
+        ])
+        return iter([data.encode("utf-8")])
