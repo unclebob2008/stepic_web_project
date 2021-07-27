c166c3da2c104e6a349f816d1ef2aa455e5fd1e5
unclebob2008 <unclebob@rambler.ru>
2021-07-27 12:36:11 +0000

    Added removing nginx default conf

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
