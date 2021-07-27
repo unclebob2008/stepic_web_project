c9cfb66dbab37dbac5ce47cd0645df0c449d785a
unclebob2008 <unclebob@rambler.ru>
2021-07-27 12:06:44 +0000

    Added wsgi

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
+cd /home/box/web
+gunicorn --bind='0.0.0.0:8080' hello:app
 #sudo /etc/init.d/mysql start
