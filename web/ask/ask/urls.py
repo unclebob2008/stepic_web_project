c98f8bf8ccba82a17aba31e2f22e5f3fa791b5b6
unclebob2008 <unclebob@rambler.ru>
2021-07-28 09:02:54 +0000

    Removed import include

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.urls import include
+#from django.urls import include
 
 urlpatterns = [
     url(r'^$', include('qa.urls'), name='home'),
