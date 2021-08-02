35fc74b61768e5656ab1d88933e9299598e8b092
unclebob2008 <unclebob@rambler.ru>
2021-08-02 12:21:19 +0000

    update python

--- init.sh
+++ init.sh
@@ -1,3 +1,12 @@
+sudo apt update
+sudo apt install python3.5 -y
+sudo apt install python3.5-dev -y
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo python3 -m pip install gunicorn
+sudo python3 -m pip install django==2.0
+sudo python3 -m pip install mysqlclient
+
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
@@ -8,7 +17,7 @@ sudo /etc/init.d/nginx restart
 #cd /home/box/web
 #gunicorn --bind='0.0.0.0:8080' hello:app
 
-cd /home/box/web/ask
-gunicorn --bind='0.0.0.0:8000' ask.wsgi
+#cd /home/box/web/ask
+#gunicorn --bind='0.0.0.0:8000' ask.wsgi
 
 sudo /etc/init.d/mysql start
