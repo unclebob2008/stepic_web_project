238e403cbfd5c38e0d002230166738bf315e84e6
unclebob2008 <unclebob@rambler.ru>
2021-08-02 08:14:12 +0000

    classes prepared

--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+	'USER': 'djuser',
+	'PASSWORD': '1234'
     }
 }
 
--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+
+class QuestionManager(models.Manager):
+    def new(self):
+	return self.order_by('-added_at')
+    def popular(self):
+	return self.order_by('-rating')
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
