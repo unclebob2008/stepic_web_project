f96de7636620226f6650bcbcd063655111938d62
unclebob2008 <unclebob@rambler.ru>
2021-08-02 09:48:44 +0000

    migration is complite

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -3,23 +3,27 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
+
 class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=100)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
-class QuestionManager(models.Manager):
-    def new(self):
-	return self.order_by('-added_at')
-    def popular(self):
-	return self.order_by('-rating')
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    question = models.ForeignKey(Question)
-    author = models.ForeignKey(User)
+    question = models.ForeignKey(
+        'Question',
+        on_delete=models.CASCADE,
+    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE,)
